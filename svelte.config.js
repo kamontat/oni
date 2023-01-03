0a1ff30d1512149a6ed18da071903608d7380384
Natcha Triratchatchawan <natcha@natchas-air.lan>
2023-01-03 22:52:56 +0700

    chore: minor changes on deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true
+		})
 	}
 };
 
