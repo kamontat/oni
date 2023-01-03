fa4a6362611f5b107318a7a71882de8ef87be96d
Natcha Triratchatchawan <natcha@natchas-air.lan>
2023-01-04 00:12:01 +0700

    fix: preprocess not compatible with unocss

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Attributify preset doesn't work with svelte-scoped: https://github.com/unocss/unocss/issues/2019
 2. Icon preset doesn't work with cdn: https://github.com/unocss/unocss/issues/2029
+3. Cannot use vitePreprocess: https://github.com/unocss/unocss/issues/2043
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+// https://github.com/unocss/unocss/issues/2043
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
 			edge: true
